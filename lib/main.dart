4a9f31566f8f7c3bacdca07e1df21f3c979594f6
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2019-04-07 01:52:26 +0200

    Center planets

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,27 @@ import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 
 void main() => runApp(MA());
 
-class Plnt {
+class Pt {
   int i;
+  int y;
   String nm;
   String fl;
   double sz;
   double rd;
   Color clr;
   String ds;
-  Plnt(this.i, Map<String, dynamic> d, this.clr) {
+  Pt(Map<String, dynamic> d, this.clr) {
     nm = d['name'];
+    y = d['y'];
     fl = d['file'];
     sz = d['sz'];
     rd = d['rd'];
     ds = d['desc'];
+    i = d['id'];
   }
 }
 
@@ -29,8 +33,9 @@ class MA extends StatefulWidget {
 
 class _MAState extends State<MA> {
   Map<String, dynamic> data;
-  List<Plnt> ps = [];
+  List<Pt> ps = [];
   bool blur;
+  final ValueNotifier<Matrix4> notifier = ValueNotifier(Matrix4.identity());
   @override
   void initState() {
     super.initState();
@@ -40,15 +45,15 @@ class _MAState extends State<MA> {
         setState(
           () {
             ps = [
-              Plnt(0, data['0'], Colors.grey),
-              Plnt(1, data['1'], Colors.orange),
-              Plnt(2, data['2'], Colors.blue),
-              Plnt(3, data['3'], Colors.red),
-              Plnt(4, data['4'], Colors.brown),
-              Plnt(5, data['5'], Colors.yellow),
-              Plnt(6, data['6'], Colors.blue),
-              Plnt(7, data['7'], Colors.cyan),
-              Plnt(8, data['8'], Colors.grey),
+              Pt(data['0'], Colors.grey),
+              Pt(data['1'], Colors.orange),
+              Pt(data['2'], Colors.blue),
+              Pt(data['3'], Colors.red),
+              Pt(data['4'], Colors.brown),
+              Pt(data['5'], Colors.yellow),
+              Pt(data['6'], Colors.blue),
+              Pt(data['7'], Colors.cyan),
+              Pt(data['8'], Colors.grey),
             ];
           },
         );
@@ -60,36 +65,40 @@ class _MAState extends State<MA> {
   Widget build(BuildContext context) {
     if (data == null) return Container();
 
-    var pStck = ps.map((p) => Orbt(p, setState) as Widget).toList();
+    var pStck = ps.map((p) => Ot(p, setState) as Widget).toList();
 
-    pStck.add(Text(
-      data['title'],
-      style: TextStyle(fontSize: 40),
-      textAlign: TextAlign.center,
-    ));
     return MaterialApp(
       theme:
           ThemeData(canvasColor: Colors.transparent, brightness: Brightness.dark, fontFamily: 'j'),
       home: Scaffold(
-        body: Center(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: SingleChildScrollView(
-              physics: ClampingScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              child: GestureDetector(
-                onTap: () {},
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.black,
-                      image: DecorationImage(image: AssetImage(data['bg']), fit: BoxFit.cover)),
-                  width: 2000,
-                  height: 3000,
-                  child: Stack(
-                    children: pStck,
+        body: MatrixGestureDetector(
+          shouldRotate: false,
+          clipChild: false,
+          onMatrixUpdate: (m, tm, sm, rm) {
+            notifier.value = m;
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.black,
+                image: DecorationImage(image: AssetImage(data['bg']), fit: BoxFit.cover)),
+            child: AnimatedBuilder(
+              animation: notifier,
+              builder: (context, child) {
+                return Transform(
+                  transform: notifier.value,
+                  child: OverflowBox(
+                    maxHeight: 5000,
+                    maxWidth: 5000,
+                    child: Container(
+                      width: 1000,
+                      height: 1000,
+                      child: Stack(
+                        children: pStck,
+                      ),
+                    ),
                   ),
-                ),
-              ),
+                );
+              },
             ),
           ),
         ),
@@ -98,31 +107,29 @@ class _MAState extends State<MA> {
   }
 }
 
-class Orbt extends HookWidget {
-  Plnt p;
-  void Function(VoidCallback fn) setState;
-  Orbt(
+class Ot extends HookWidget {
+  Pt p;
+  void Function(VoidCallback fn) ss;
+  Ot(
     this.p,
-    this.setState, {
+    this.ss, {
     Key key,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final AnimationController controller = useAnimationController(
-      duration: Duration(seconds: p.i * 4 + 2),
-      lowerBound: 2.8,
-      upperBound: 5,
+    final AnimationController ctrl = useAnimationController(
+      duration: Duration(seconds: p.y),
+      lowerBound: 0,
+      upperBound: 6.28,
     );
     useEffect(() {
-      controller.repeat();
-    }, [controller]);
-    double value = useAnimation(controller);
+      ctrl.repeat();
+    }, [ctrl]);
+    double value = useAnimation(ctrl);
     return Stack(
       children: <Widget>[
-        Positioned(
-          right: -p.rd / 2,
-          bottom: -p.rd / 2,
-          child: IgnorePointer(
+        IgnorePointer(
+          child: Center(
             child: AnimatedContainer(
               duration: Duration(seconds: 1),
               width: p.rd,
@@ -132,13 +139,11 @@ class Orbt extends HookWidget {
             ),
           ),
         ),
-        Positioned(
-          right: 0,
-          bottom: 0,
+        Center(
           child: Transform(
             transform: Matrix4.translationValues(
-              p.rd / 2 * cos(value) + p.sz / 2,
-              p.rd / 2 * sin(value) + p.sz / 2,
+              p.rd / 2 * cos(value),
+              p.rd / 2 * sin(value),
               0,
             ),
             child: AnimatedContainer(
@@ -146,13 +151,12 @@ class Orbt extends HookWidget {
               width: p.sz,
               height: p.sz,
               child: RotationTransition(
-                turns: controller,
+                turns: ctrl,
                 child: GestureDetector(
                     onTap: () {
                       Navigator.of(context).push(
                         PageRouteBuilder(
                           opaque: false,
-                          // transitionDuration: Duration(milliseconds: 600),
                           barrierDismissible: true,
                           pageBuilder: (context, _, __) {
                             return GestureDetector(
@@ -171,7 +175,10 @@ class Orbt extends HookWidget {
                                             style: TextStyle(fontSize: 40, fontFamily: 'b'),
                                           ),
                                           Hero(tag: p.i, child: Image.asset(p.fl)),
-                                          txt(p.ds),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(p.ds),
+                                          ),
                                         ],
                                       ),
                                     ),
@@ -194,13 +201,6 @@ class Orbt extends HookWidget {
           ),
         ),
       ],
-    );
-  }
-
-  Padding txt(String d) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(d),
     );
   }
 }
