3f87c7f64faeb74ac392307226f3de7d7913c512
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2019-04-06 12:03:33 +0200

    Add hero transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() => runApp(MA());
 
@@ -8,14 +9,15 @@ class MA extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HP(),
+      theme: ThemeData(canvasColor: Colors.transparent),
+      home: HomePage(),
     );
   }
 }
 
-class HP extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _HPState createState() => _HPState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class Plnt {
@@ -25,18 +27,16 @@ class Plnt {
   double rd;
   double pd;
   Color clr;
-  Animation<double> an;
-  bool v = false;
+  double op;
   Plnt(this.i, this.nm, this.sz, this.rd, this.clr);
 }
 
-class _HPState extends State<HP> with SingleTickerProviderStateMixin {
-  AnimationController ctrl;
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   List<Plnt> ps = [];
+  bool blur;
   @override
   void initState() {
     super.initState();
-    ctrl = AnimationController(duration: const Duration(seconds: 20), vsync: this);
     ps = [
       Plnt(0, 'uran', 16, 200, Colors.grey),
       Plnt(1, 'venus', 38, 480, Colors.orange),
@@ -47,31 +47,17 @@ class _HPState extends State<HP> with SingleTickerProviderStateMixin {
       Plnt(6, 'earth', 40, 2500, Colors.purple),
       Plnt(7, 'pluto', 20, 3050, Colors.grey),
     ];
-    ps.forEach((f) => addTween(f));
-    ctrl.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    var pStck = ps.map((p) => Orbt(p, ctrl, setState) as Widget).toList();
-    if (ps.any((n) => n.v == true)) {
-      pStck.add(IgnorePointer(
-        child: ClipRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-            child: Container(
-              decoration: BoxDecoration(color: Colors.black26),
-            ),
-          ),
-        ),
-      ));
-    }
+    var pStck = ps.map((p) => Orbt(p, setState) as Widget).toList();
 
     pStck.add(AnimatedOpacity(
-      opacity: ctrl.value,
+      opacity: 1,
       duration: Duration(seconds: 10),
       child: Text(
-        "\nLet' go for a ride through space!",
+        "\nLet's go for a ride through space!",
         style: TextStyle(fontSize: 40, color: Colors.white, fontStyle: FontStyle.italic),
         textAlign: TextAlign.center,
       ),
@@ -82,10 +68,7 @@ class _HPState extends State<HP> with SingleTickerProviderStateMixin {
           physics: ClampingScrollPhysics(),
           scrollDirection: Axis.vertical,
           child: GestureDetector(
-            onTap: () {
-              ctrl.forward();
-              ps.forEach((n) => n.v = false);
-            },
+            onTap: () {},
             child: Container(
               decoration: BoxDecoration(color: Colors.black),
               width: 400,
@@ -99,70 +82,40 @@ class _HPState extends State<HP> with SingleTickerProviderStateMixin {
       ),
     );
   }
-
-  void addTween(Plnt f) {
-    var begin = f.i > 2 ? atan(400 / f.rd) : pi;
-    var tween = Tween<double>(begin: 1.35 * pi - begin, end: 1.55 * pi).animate(
-      CurvedAnimation(
-        parent: ctrl,
-        curve: Interval(
-          f.i % 2 == 0 ? 0.0 : 0.1,
-          f.i % 2 == 0 ? 0.9 : 1.0,
-        ),
-      ),
-    )
-      ..addListener(() {
-        setState(() {});
-      })
-      ..addStatusListener((AnimationStatus status) {
-        if (status == AnimationStatus.completed) {
-          ctrl.repeat();
-        }
-      });
-    f.an = tween;
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    ctrl.dispose();
-  }
 }
 
-class Orbt extends StatelessWidget {
+class Orbt extends HookWidget {
   Plnt p;
-  AnimationController ctrl;
   void Function(VoidCallback fn) setState;
   Orbt(
     this.p,
-    this.ctrl,
     this.setState, {
     Key key,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final AnimationController controller = useAnimationController(
+      duration: Duration(seconds: 10),
+      lowerBound: 2.8,
+      upperBound: 5,
+    );
+    useEffect(() {
+      controller.repeat();
+    }, [controller]);
+    double value = useAnimation(controller);
+
     return Stack(
       children: <Widget>[
-        // Positioned(
-        //   right: 0,
-        //   bottom: 0,
-        //   child: CustomPaint(
-        //     foregroundPainter: MyPainter(p.clr, p.sz, p.an),
-        //     child: SizedBox(
-        //       width: p.rd,
-        //       height: p.rd,
-        //     ),
-        //   ),
-        // ),
         Positioned(
-          right: -p.rd/2,
-          bottom: -p.rd/2,
-          child: Container(
-            width: p.rd,
-            height: p.rd,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(color: p.clr, width: 1)
+          right: -p.rd / 2,
+          bottom: -p.rd / 2,
+          child: IgnorePointer(
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              width: p.rd,
+              height: p.rd,
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, border: Border.all(color: p.clr, width: 1)),
             ),
           ),
         ),
@@ -171,64 +124,51 @@ class Orbt extends StatelessWidget {
           bottom: 0,
           child: Transform(
             transform: Matrix4.translationValues(
-                p.rd / 2 * cos(p.an.value) + p.sz / 2, p.rd / 2 * sin(p.an.value) + p.sz / 2, 0),
-            child: Container(
+              p.rd / 2 * cos(value) + p.sz / 2,
+              p.rd / 2 * sin(value) + p.sz / 2,
+              0,
+            ),
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
               width: p.sz,
               height: p.sz,
               child: RotationTransition(
-                turns: p.an,
+                turns: controller,
                 child: GestureDetector(
-                  onTap: () async {
-                    if (ctrl.velocity > 0.02)
-                      ctrl.stop();
-                    else
-                      ctrl.forward();
-                    setState(() {
-                      p.v = !p.v;
-                    });
-                  },
-                  child: Image.asset(
-                    'assets/${p.nm}.png',
-                  ),
-                ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        PageRouteBuilder(
+                          opaque: false,
+                          barrierDismissible: true,
+                          pageBuilder: (context, _, __) {
+                            return BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                              child: Scaffold(
+                                body: Center(
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(p.nm, style: TextStyle(color: Colors.white)),
+                                      Hero(tag: p.i, child: Image.asset('assets/${p.nm}.png'))
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    child: Hero(
+                      tag: p.i,
+                      child: Image.asset(
+                        'assets/${p.nm}.png',
+                      ),
+                    )),
               ),
             ),
           ),
         ),
       ],
     );
-  }
-}
-
-class FadeRouteBuilder<T> extends PageRouteBuilder<T> {
-  final Widget p;
-  FadeRouteBuilder({@required this.p})
-      : super(
-          pageBuilder: (context, a1, a2) => p,
-          transitionsBuilder: (context, a1, a2, child) {
-            return FadeTransition(opacity: a1, child: child);
-          },
-        );
-}
-
-class MyPainter extends CustomPainter {
-  Color lC;
-  double w;
-  Animation<double> a;
-  MyPainter(this.lC, this.w, this.a);
-  @override
-  void paint(Canvas c, Size s) {
-    Paint l = new Paint()
-      ..color = lC.withAlpha(80)
-      ..strokeCap = StrokeCap.round
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2;
-    Rect r = new Rect.fromLTWH(s.width / 2, s.height / 2, s.width, s.height);
-    c.drawOval(r, l);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oD) {
-    return true;
   }
 }
